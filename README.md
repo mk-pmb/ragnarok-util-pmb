dd9995ee3aa5c14cf00ffa86facfcba4379072b8
mk-pmb <mk-pmb@users.noreply.github.com>
2015-02-18 21:58:22 +0100

    extract seems to work

--- README.md
+++ README.md
@@ -12,17 +12,32 @@ BGM/
 BGM/53.mp3
 ```
 * list archive content details:
+  * first row is hex byte offset of file record in the un-gzip-ed stream.
 ```bash
 $ ragtar tvzf 2014-09-16_aPatchClient.rgz
-      / PatchClient
-      / PatchClient/Lua Files
-      / PatchClient/Lua Files/ServerInfoz
-   1851 PatchClient/Lua Files/ServerInfoz/ServerInfo_FR.lub
-      / PatchClient/Lua Files/SkinInfoz
-   4916 PatchClient/Lua Files/SkinInfoz/SkinInfo_FR.lub
-  31560 PatchClient/Replay.bmp
-      = 4 directories, 3 files, 38327 bytes
+@0000000      / PatchClient
+@000000e      / PatchClient/Lua Files
+@0000026      / PatchClient/Lua Files/ServerInfoz
+@000004a   1851 PatchClient/Lua Files/ServerInfoz/ServerInfo_FR.lub
+@00007bf      / PatchClient/Lua Files/SkinInfoz
+@00007e1   4916 PatchClient/Lua Files/SkinInfoz/SkinInfo_FR.lub
+@0001b4b  31560 PatchClient/Replay.bmp
+@00096b0      = 4 directories, 3 files, 38327 bytes
 ```
+* extract files:
+```bash
+$ ragtar xvzf 2014-09-16_aPatchClient.rgz --exist=skip
+dir exists:     PatchClient
+dir exists:     PatchClient/Lua Files
+dir exists:     PatchClient/Lua Files/ServerInfoz
+skip file:      PatchClient/Lua Files/ServerInfoz/ServerInfo_FR.lub
+create dir:     PatchClient/Lua Files/SkinInfoz
+create file:    PatchClient/Lua Files/SkinInfoz/SkinInfo_FR.lub
+skip file:      PatchClient/Replay.bmp
+$ ragtar xvzf 2013-02-28_data_gm.rgz --exist=replace
+replace file:   patch.inf
+```
+
 
 
 
--- patcher/ragtar.py
+++ patcher/ragtar.py
@@ -10,12 +10,16 @@ from sys import stdin, stdout, stderr, argv
 from codecs import open as cfopen
 from gzip import open as gzopen
 maybe_gz_open = {True:gzopen, False:open}
-from os import SEEK_CUR
-from os.path import realpath, dirname, join as joinpath, exists as fexists
+from os import SEEK_CUR, mkdir
+from os.path import normpath, exists as fexists, isdir
+
+
+def noop(*ignore, **kwignore):
+    pass
 
 
 def main(invocation, *cli_args):
-    args_iter = iter(cli_args)
+    arg_opts, args_iter = filter_arg_opts(cli_args)
     arg_modes = args_iter.next()
     arg_action_names = {
         'A': 'concatenate',
@@ -34,27 +38,42 @@ def main(invocation, *cli_args):
         raise RuntimeError('no action given', arg_action_names)
     else:
         raise dunnohow('multiple actions', arg_action)
+    if 'v' in arg_modes:
+        arg_opts()['verbose'] = True
 
-    if arg_action in ('list'):
+    if arg_action in ('list', 'extract'):
         pass
     else:
         raise dunnohow('action ' + arg_action)
 
-
     src_fn = '<stdin>'
     src_stream = stdin
     log_stream = stdout
+    def logp(fmt, *args, **kwargs):
+        print >>log_stream, fmt.format(*args, **kwargs)
+    if arg_opts('verbose'):
+        logv = logp
+    else:
+        logv = noop
+
     if 'f' in arg_modes:
         src_fn = args_iter.next()
         src_stream = maybe_gz_open['z' in arg_modes](src_fn, 'rb')
     elif 'z' in arg_modes:
         raise dunnohow('stdin zlib')
 
+    item_addr = '?'
     stats = { 'dirs': 0, 'files': 0, 'bytes': 0 }
     while True:
+        item_addr = '@{:07x}'.format(src_stream.tell())
         item_type = src_stream.read(1)
         item_size = ord(src_stream.read(1))
-        item_name = defuse_filename(src_stream.read(item_size))
+        item_name = src_stream.read(item_size).split('\x00')
+        if len(item_name) < 1:
+            logp('W: {}: filename not zero-terminated', item_addr)
+        if (len(item_name) != 2) or (item_name[-1] != ''):
+            logp('W: {}: unexpected zero-byte in filename', item_addr)
+        item_name = defuse_filename(item_name[0])
         item_size = '?'
         if item_type == 'e':
             break
@@ -65,18 +84,36 @@ def main(invocation, *cli_args):
         elif item_type == 'd':
             stats['dirs'] += 1
             item_size = '/'
-        if 'v' in arg_modes:
-            print >>log_stream, '{:>7}\t{}'.format(item_size, item_name)
-        if arg_action == 'list':
-            if 'v' not in arg_modes:
-                print >>log_stream, item_name + {True: '/'
-                    }.get(item_type == 'd', '')
+        if arg_action == 'extract':
+            if item_type == 'd':
+                if isdir(item_name):
+                    logv('dir exists:\t{}', item_name)
+                else:
+                    logv('create dir:\t{}', item_name)
+                    mkdir(item_name)
+            elif item_type == 'f':
+                item_replace = check_replace_dest(item_name, arg_opts)
+                logv('{1} file:\t{0}', item_name, { None: 'create',
+                        True: 'replace', False: 'skip' }[item_replace])
+                if item_replace in (True, None):
+                    copy_stream_blocks(src_stream, item_name, bytes=item_size,
+                                       opts=arg_opts)
+                else:
+                    src_stream.seek(item_size, SEEK_CUR)
+            else:
+                raise dunnohow('extract item type ' + item_type)
+        if arg_action in ('list',):
+            if arg_opts('verbose'):
+                logp('{} {:>6}\t{}', item_addr, item_size, item_name)
+            else:
+                logp('{}{}', item_name, {'d':'/', 'f':''}.get(item_type, '?'))
+        if arg_action in ('list',):
             if item_type == 'f':
                 src_stream.seek(item_size, SEEK_CUR)
 
-    if 'v' in arg_modes:
-        print ('{:>7}\t{dirs} directories, {files} files, {bytes} bytes'
-              ).format('=', **stats)
+    if arg_action in ('list',):
+        logv('{} {:>6}\t{dirs} directories, {files} files, {bytes} bytes',
+             item_addr, '=', **stats)
     return
 
 
@@ -93,19 +130,97 @@ def dunnohow(msg, *args, **kwargs):
     return NotImplementedError(msg + ': not supported yet', *args, **kwargs)
 
 
-def defuse_filename(fn):
-    fn = str(fn)
-    if fn == '':
+def filter_arg_opts(args_iter):
+    opts = {}
+    files = []
+    for arg in args_iter:
+        if arg.startswith('--'):
+            if arg == '--':
+                files += list(args_iter)
+                break
+            arg = arg[2:].split('=', 1)
+            if len(arg) < 2:
+                arg.append(True)
+            opts[arg[0]] = arg[1]
+        else:
+            files.append(arg)
+    def getopts(key=None, dflt=None):
+        if key is None:
+            return opts
+        return opts.get(key, dflt)
+    return getopts, iter(files)
+
+
+def defuse_filename(orig_fn):
+    orig_fn = str(orig_fn)
+    if orig_fn == '':
         return '__E_NO_FILENAME__'
-    if len(fn) > 1:
-        return ''.join([defuse_filename(ch) for ch in fn])
-    if fn == '\\':
-        return '/'
-    if fn == '\x00':
-        return ''
-    if fn.isalnum() or (fn in ' ._-@,'):
-        return fn
-    return '%{:02X}'.format(ord(fn))
+
+    def esc_char(ch):
+        if ch == '\\':
+            return '/'
+        if ch == '\x00':
+            return ''
+        if ch.isalnum() or (ch in ' ._-@,'):
+            return ch
+        return '%{:02X}'.format(ord(ch))
+
+    safe_fn = ''.join([esc_char(ch) for ch in orig_fn])
+    safe_fn = normpath(safe_fn)
+    for suspi in ('./', '/..',):
+        if suspi in safe_fn:
+            raise ValueError('suspicious substring found in filename',
+                             suspi, safe_fn)
+
+    return safe_fn
+
+
+def copy_stream_blocks(src, dest, bytes, opts=None):
+    if opts is None:
+        opts = lambda opt, dflt: dflt
+    elif isinstance(opts, dict):
+        opts = opts.get
+    bytes_remain = bytes
+    cleanup = None
+
+    if isinstance(dest, basestring):
+        output = open(dest, 'wb')
+        cleanup = output.close
+        output = output.write
+    else:
+        output = dest.write
+
+    blocksize = long(opts('blocksize', 4096))
+
+    while bytes_remain > 0:
+        buf = src.read(min(bytes_remain, blocksize))
+        bytes_remain -= len(buf)
+        output(buf)
+    if cleanup is not None:
+        cleanup()
+
+
+def check_replace_dest(fn, opts):
+    if not fexists(fn):
+        return None
+    strat = opts('exist')
+    if strat == 'skip':
+        return False
+    if strat == 'replace':
+        return True
+    raise IOError('destination file already exists', fn)
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
